33271e75d528dc201579068b427601711453872d
Daniel <daniel65536@gmail.com>
2015-11-22 04:01:58 +0800

    compress favicons

---
+++ source/_process.sh
@@ -0,0 +1,9 @@
+convert _origin.png -resize 256x256 apple-touch-icon.png
+optipng -o7 apple-touch-icon.png
+
+convert _origin.png  -bordercolor white -border 0 \
+    \( -clone 0 -resize 16x16 \) \
+    \( -clone 0 -resize 32x32 \) \
+    \( -clone 0 -resize 48x48 \) \
+    \( -clone 0 -resize 64x64 \) \
+    -delete 0 -alpha off -colors 256 favicon.ico
