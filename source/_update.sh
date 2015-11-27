f5e9517afd425518876516ebd5793b0f54748e34
Daniel <daniel65536@gmail.com>
2015-11-27 14:29:59 +0800

    combine favicons

--- source/_update.sh
+++ source/_update.sh
@@ -1,11 +1,6 @@
-convert _origin.png -resize 256x256 apple-touch-icon.png
-optipng -o7 apple-touch-icon.png
-
-convert _origin.png  -bordercolor white -border 0 \
-    \( -clone 0 -resize 16x16 \) \
-    \( -clone 0 -resize 32x32 \) \
-    \( -clone 0 -resize 48x48 \) \
-    \( -clone 0 -resize 64x64 \) \
-    -delete 0 -alpha off -colors 256 favicon.ico
+if [[ ! -f favicon.png ]]; then
+    convert _origin.png -resize 256x256 favicon.png
+    optipng -o7 favicon.png
+fi
 
 echo $RANDOM; sed -i "" "s/#.*/# $(printf %04x $RANDOM $RANDOM)/g" mono.appcache
