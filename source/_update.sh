a6d7e136d6104f6250ae0f675de295572c60b1cb
Daniel <daniel65536@gmail.com>
2016-07-17 17:20:15 +0800

    reduce css size

--- source/_update.sh
+++ source/_update.sh
@@ -1,6 +1,6 @@
 if [[ ! -f favicon.png ]]; then
-    convert _origin.png -resize 256x256 favicon.png
-    optipng -o7 favicon.png
+    convert _origin.png -resize 256x256 -colors 256 favicon.png
+    optipng -o7 -zm1-9 -strip all favicon.png
 fi
 
 echo $RANDOM; sed -i "" "s/#.*/# $(printf %04x $RANDOM $RANDOM)/g" mono.appcache
