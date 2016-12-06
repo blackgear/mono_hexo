8ed690f9abf08f3fa74e5a2b6475dac189ac9c5a
Daniel <daniel65536@gmail.com>
2016-12-06 23:01:46 +0800

    major change on details

--- source/_update.sh
+++ source/_update.sh
@@ -1,6 +1,5 @@
-if [[ ! -f favicon.png ]]; then
-    convert _origin.png -resize 256x256 -colors 256 favicon.png
-    optipng -o7 -zm1-9 -strip all favicon.png
-fi
-
-echo $RANDOM; sed -i "" "s/#.*/# $(printf %04x $RANDOM $RANDOM)/g" mono.appcache
+#!/usr/bin/env bash
+# -*- coding: utf-8 -*-
+echo $RANDOM;
+convert _origin.png -resize 256x256 +dither -colors 64 -strip favicon.ico
+sed -i "" "s/#.*/# $(printf %04x $RANDOM $RANDOM)/g" mono.appcache
