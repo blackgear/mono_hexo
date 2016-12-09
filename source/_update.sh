633268b960d58b3af3c00230db1d21f0a7fa2e16
Daniel <daniel65536@gmail.com>
2016-12-10 01:32:10 +0800

    improve favicon

--- source/_update.sh
+++ source/_update.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 # -*- coding: utf-8 -*-
-echo $RANDOM;
-convert _origin.png -resize 256x256 +dither -colors 64 -strip favicon.ico
-sed -i "" "s/#.*/# $(printf %04x $RANDOM $RANDOM)/g" mono.appcache
+
+convert _origin.png -background white -alpha remove \
+    \( -clone 0 -resize 16x16 -fuzz 10% -transparent white \) \
+    \( -clone 0 -resize 32x32 -fuzz 10% -transparent white \) \
+    \( -clone 0 -resize 48x48 -fuzz 10% -transparent white \) \
+    \( -clone 0 -resize 256x256 -transparent white \) \
+    -delete 0 +dither -colors 64 -depth 8 favicon.ico
+
+sed -i "" "s/#.*/#$(printf %x $(date +%s))/g" mono.appcache
