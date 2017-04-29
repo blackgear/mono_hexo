a525f0c2159069562f3143ce5f0128cdad9fcc3f
Daniel <daniel65536@gmail.com>
2017-04-29 15:17:05 +0800

    fix newline

--- source/_update.sh
+++ source/_update.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
 # -*- coding: utf-8 -*-
 
-convert _origin.png -resize 256x256 +dither -colors 64 -depth 8 favicon.png
-optipng -o7 -zm1-9 -strip all favicon.png
+[ ! -f favicon.png ] && convert _origin.png -resize 256x256 +dither -colors 64 -depth 8 favicon.png && optipng -o7 -zm1-9 -strip all favicon.png
 
-sed -i "" "s/#.*/#$(printf %x $(date +%s))/g" mono.appcache
+cat << _EOF_ > mono.appcache
+CACHE MANIFEST
+#$(printf %x $(date +%s))
+mono.css
+favicon.ico
+NETWORK:
+*
+_EOF_
